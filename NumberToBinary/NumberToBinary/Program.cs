0a3ed8451588124668a625ef6d6ba755a040eb35
tonyj2007 <tonyj2007@users.noreply.github.com>
2017-02-16 18:24:52 +0000

    Add files via upload

---
+++ NumberToBinary/NumberToBinary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            int modulas;
+            String binary = "";
+            while (input >= 1)
+            {
+                modulas = input % 2;
+                binary = modulas + binary;
+                input /= 2;
+            }
+            Console.Write(binary);
+            Console.ReadLine();
+        }
+    }
+}
